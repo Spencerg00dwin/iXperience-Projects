73e165f3f31eed6bcab3558314b2622e0e5341af
Spencer Goodwin <sigood@umich.edu>
2022-07-18 15:26:38 -0400

    Day 6

---
+++ Day-6/task-list/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import BookTable from './component/BookTable'
+import AddBook from './component/AddBook'
+
+
+export default function App() {
+
+  return (
+    <div>
+
+    <AddBook></AddBook>
+
+    <BookTable></BookTable>
+
+    </div>
+
+
+  )
+}
+
